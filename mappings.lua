e86ea89d97ac2fee31e21df3c829b3ac7c0a0d0c
faea726 <fantasy.eagle96@gmail.com>
2023-05-06 00:48:33 +0700

    open link

--- mappings.lua
+++ mappings.lua
@@ -1,9 +1,30 @@
 ---@type MappingsTable
 local M = {}
 
+function open_url()
+  local url = vim.fn.expand "<cfile>"
+
+  if url:sub(1, 4) ~= "http" then
+    -- Display notification if text is not a link
+    vim.notify("'" .. url .. "' is not a link", "warn", { title = "Neovim" })
+    return
+  end
+
+  local command = ""
+  if vim.fn.has "win32" == 1 then
+    command = "start"
+  elseif vim.fn.has "mac" == 1 then
+    command = "open"
+  elseif vim.fn.has "unix" == 1 then
+    command = "xdg-open"
+  end
+  vim.fn.system(command .. " " .. url)
+end
+
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["gx"] = { ":lua open_url()<CR>", "open link" },
   },
 }
 
